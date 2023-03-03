6846981d1ace7eba24448dafa1ffe121d2109864
antonyjes <72809149@continental.edu.pe>
2023-03-02 21:55:43 -0500

    File storage

--- server/index.js
+++ server/index.js
@@ -10,3 +10,48 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { v4 as uuidv4 } from "uuid";
 
+//CONFIGURATIONS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+mongoose.set("strictQuery", true);
+
+//FILE STORAGE
+const adminStorage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, "public/assets/admins");
+    },
+    filename: function (req, file, cb){
+        const uniqueSuffix = Date.now() + "-" + uuidv4();
+        cb(null, uniqueSuffix + "-" + file.originalname);
+    }
+});
+
+const teacherStorage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, "public/assets/teachers");
+    },
+    filename: function (req, file, cb){
+        const uniqueSuffix = Date.now() + "-" + uuidv4();
+        cb(null, uniqueSuffix + "-" + file.originalname);
+    }
+});
+
+const studentStorage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, "public/assets/students");
+    },
+    filename: function (req, file, cb){
+        const uniqueSuffix = Date.now() + "-" + uuidv4();
+        cb(null, uniqueSuffix + "-" + file.originalname);
+    }
+});
