c73ac278f10be01281abb5e8685526c8292858e4
antonyjes <72809149@continental.edu.pe>
2023-03-02 21:14:58 -0500

    Auth token, import packages, env

--- server/index.js
+++ server/index.js
@@ -1,2 +1,12 @@
-console.log("Server starts");
-console.log("Change")
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import { v4 as uuidv4 } from "uuid";
+
