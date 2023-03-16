28be5fc5d12e8065d1bb54f2a58b81cc5df06f79
antonyjes <72809149@continental.edu.pe>
2023-03-15 22:11:17 -0500

    New component for Student Record

---
+++ client/src/components/StudentAttendance.jsx
@@ -0,0 +1,16 @@
+import Aside from "./Aside";
+import Sidebar from "./Sidebar";
+
+const StudentAttendance = () => {
+    return(
+        <>
+            <Sidebar />
+            <Aside />
+            <div>
+                Studdent Attendance
+            </div>
+        </>
+    )
+}
+
+export default StudentAttendance;
